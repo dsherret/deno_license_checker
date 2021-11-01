33ef735169e6181e6ed179c897f210decccb92d3
David Sherret <dsherret@gmail.com>
2021-11-01 12:28:05 -0400

    Working dnt.

--- build_npm.ts
+++ build_npm.ts
@@ -1,4 +1,8 @@
-import { build } from "https://deno.land/x/dnt/mod.ts";
+import { build } from "https://deno.land/x/dnt@0.1.3/mod.ts";
+import { copy } from "https://deno.land/std@0.113.0/fs/mod.ts";
+
+await copy("testdata", "npm/esm/testdata", { overwrite: true });
+await copy("testdata", "npm/umd/testdata", { overwrite: true });
 
 await build({
   entryPoints: ["./main.ts"],
@@ -21,3 +25,9 @@ await build({
     },
   },
 });
+
+Deno.writeTextFileSync(
+  "./npm/.npmignore",
+  "./npm/esm/testdata\n./npm/umd/testdata\n",
+  { append: true },
+);
